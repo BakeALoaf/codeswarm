1396faa26598cfface75427fb329e542bd596032
James Barker <jbarkerlouth@gmail.com>
2023-03-07 23:20:31 +0100

    not updating database

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,21 +1,15 @@
 class DashboardsController < ApplicationController
-
-  def new
-    @user = User.new
-  end
-
   def index
     @users = User.all
   end
 
-
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to dashboards_path, notice: "Admin status updated."
-    else
-      render :new
+    debugger
+      user_params.each do |id, user_params|
+      user = User.find(id)
+      user.update(user_params)
     end
+    redirect_to dashboard_path, notice: "Admin status updated."
   end
 
   private
